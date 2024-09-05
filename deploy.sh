68ae56df1f150f05527eaacfbb3c216e76060b88
Mark Szymanski <mark.szymanski@socotra.com>
2024-09-05 18:28:57 -0400

    remove obsolete sample scripts

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Sample script for Enterprise Core config archive deployment, using a personal access token (PAT)
-
-curl --location "https://api-base-url/config/$1/deployments/deploy" \
---header "Authorization: Bearer $2" \
---form 'file=@"./config.zip"'
-
