ad0149aa0ce5474313d6dc1c53430a895bb80d9d
Lydia Ahrens <lydiamidori@gmail.com>
2024-12-12 19:04:42 -0500

    gl rater for new business completed

--- socotra-config/products/GeneralLiability/plugins/java/GLRatingPlugin.java
+++ socotra-config/products/GeneralLiability/plugins/java/GLRatingPlugin.java
@@ -49,9 +49,6 @@ public class GLRatingPlugin implements RatePlugin {
         log.info("exposure");
         log.info(Integer.toString(exposure));
 
-        int limitOfInsuranceBI =   100000;
-        int limitOfInsurancePD =  100000;
-
         // retrieve value, cell L17
         int premiumBase = tableRecordFetcher.getTable(ClassCodes.class).getRecord(ClassCodes.makeKey(classCode)).orElseThrow().premiumBase();
         log.info(Integer.toString(premiumBase));
@@ -64,16 +61,62 @@ public class GLRatingPlugin implements RatePlugin {
 
         // retrieve lcm factor, cell I19
         BigDecimal baseRateLCM = tableRecordFetcher.getTable(LCM.class).getRecord(LCM.makeKey(state)).orElseThrow().premOpsLCM();
+        log.info("base rate lcm");
         log.info(baseRateLCM.toString());
 
-        // TODO: potentially need to sum these values
         BigDecimal baseRateTerritory = tableRecordFetcher.getTable(TerritoryRelativity.class).getRecord(TerritoryRelativity.makeKey(state, territory)).orElseThrow().premOpsFactors();
-        // TODO: limit of liability SUMIFS
-        // TODO: deductible SUMIFS
-        // TODO: discount factors
-        // TODO: affinity discount
+        log.info("base rate territory");
+        log.info(baseRateTerritory.toString());
 
-        BigDecimal testDiscountFactor=new BigDecimal(".970");
+        // TODO: limit of liability SUMIFS
+        BigDecimal limitOfInsuranceAgg = quote.location().limitOfInsuranceAggregate().value();
+        log.info("limit of insurance agg");
+        log.info(limitOfInsuranceAgg.toString());
+        BigDecimal limitOfInsurancePerOccurrenceBI = quote.location().limitOfInsurancePerOccurrenceBI().value();
+        BigDecimal limitOfInsurancePerOccurrencePD = quote.location().limitOfInsurancePerOccurrencePD().value();
+        BigDecimal limitOfInsuranceMed = quote.location().limitOfInsuranceMedical().value();
+
+        BigDecimal lolRate = new BigDecimal("0.0");
+        BigDecimal medicalRate  = new BigDecimal("0.0");
+        BigDecimal lolFinalRate = new BigDecimal("0.0");
+        if(limitOfInsurancePerOccurrenceBI.compareTo(limitOfInsurancePerOccurrencePD) == 0) {
+            lolRate = tableRecordFetcher.getTable(IFL14.class).getRecord(IFL14.makeKey(limitOfInsurancePerOccurrenceBI, limitOfInsuranceAgg)).orElseThrow().factorIFL();
+            log.info("lol rate");
+            log.info(lolRate.toString());
+
+            medicalRate = tableRecordFetcher.getTable(IFL23.class).getRecord(IFL23.makeKey(limitOfInsuranceMed)).orElseThrow().factor();
+
+            lolFinalRate = lolRate.multiply(medicalRate);
+        }
+        log.info("lol final rate");
+        log.info(lolFinalRate.toString());
+
+        // deductible bi
+        // deudcitble pd
+        // deductible bipd
+
+        BigDecimal dedBI = quote.location().deductible_BI().value();
+        log.info("ded-bi val");
+        log.info(dedBI.toString());
+        BigDecimal dedPD = quote.location().deductible_PD().value();
+        BigDecimal dedBIPD = quote.location().deductible_BIPD().value();
+//
+        BigDecimal dedBIRate = tableRecordFetcher.getTable(DeductibleBI.class).getRecord(DeductibleBI.makeKey(dedBI)).orElseThrow().factorBI();
+        log.info("ded-bi rate");
+        log.info(dedBIRate.toString());
+        BigDecimal dedPDRate = tableRecordFetcher.getTable(DeductiblePD.class).getRecord(DeductiblePD.makeKey(dedPD)).orElseThrow().factorPD();
+        BigDecimal dedBIPDRate = tableRecordFetcher.getTable(DeductibleBIPD.class).getRecord(DeductibleBIPD.makeKey(dedBIPD)).orElseThrow().factorBIPD();
+
+        BigDecimal finalDedRate = dedBIRate.multiply(dedPDRate).multiply(dedBIPDRate);
+        log.info("deductible rate");
+        log.info(finalDedRate.toString());
+
+
+        // TODO: discount factors, tenure is for renewals only
+
+        // affinity discount
+        String affinityDiscount = quote.data().glQuestions().affinityDiscount();
+        BigDecimal affinityRate = tableRecordFetcher.getTable(DiscountFactorsAffinity.class).getRecord(DiscountFactorsAffinity.makeKey(affinityDiscount)).orElseThrow().factor();
 
         // will always be single as this is the GL rater
         String packageGrp = "Single";
@@ -88,19 +131,21 @@ public class GLRatingPlugin implements RatePlugin {
         String terrorism = "Yes";
         BigDecimal terrorismRate = tableRecordFetcher.getTable(Terrorism.class).getRecord(Terrorism.makeKey(terrorism)).orElseThrow().factor();
 
-        BigDecimal finalRate = terrorismRate.multiply(claimsRate).multiply(packageRate).multiply(baseRateTerritory).multiply(baseRateLCM).multiply(baseRate).multiply(baseRateLCM).multiply(testDiscountFactor);
+        BigDecimal finalRate = terrorismRate.multiply(claimsRate).multiply(packageRate).multiply(baseRateTerritory).multiply(baseRateLCM).multiply(baseRate).multiply(baseRateLCM).multiply(lolFinalRate).multiply(finalDedRate).multiply(affinityRate);
         log.info("finalRate");
         log.info(finalRate.toString());
 
         BigDecimal minimumPremium = new BigDecimal("500.0");
+        BigDecimal months =new BigDecimal("12.0");
+
         if (finalRate.compareTo(minimumPremium) <= 0) {
+            BigDecimal monthlyAmount = minimumPremium.divide(months);
             ratingItems.add(RatingItem.builder()
                     .elementLocator(quote.locator())
                     .chargeType(ChargeType.premium)
-                    .rate(BigDecimal.valueOf(41.67))
+                    .rate(monthlyAmount)
                     .build());
         } else {
-            BigDecimal months =new BigDecimal("12.0");
             BigDecimal monthlyAmount = finalRate.divide(months);
             ratingItems.add(RatingItem.builder()
                     .elementLocator(quote.locator())
